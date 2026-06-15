c660f245d8ac6c4f8215758cc1bfb02b4bfa440c
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2026-06-16 00:13:45 +0300

    Add attendance records retrieval and search functions

--- -WorkTimeTracker/Server/Middleware/Attendance_Mid.js
+++ -WorkTimeTracker/Server/Middleware/Attendance_Mid.js
@@ -1,6 +1,28 @@
 let tableName="attendance";
 
 
+async function GetAllItems(req,res,next){
+
+    let values = [];
+    let Query = `SELECT * FROM ${tableName}  `;
+    Query += " ORDER BY  employee_id  ";
+   
+    res.ok=false;
+    res.err="";
+    let rows= await GenObj_Mid.QueryExecSimpleReply(Query,values);
+    if(rows === false){
+        res.err="חלה תקלה, נא לנסות שנית";
+        return res.status(500).json({status:"ERROR",Query: Query,err:res.err,values:values});
+    }
+    let data_by_id=[];
+    for(let row of rows){
+        data_by_id[row.id]=row.employee_id;
+    }
+    res.ok=true;
+    req.ItemsData={list:rows,data_by_id:data_by_id};
+
+    next();
+}
 
 async function AddItem(req,res,next){
     let employee_id   =  req.body.employee_id   || "";
@@ -54,10 +76,43 @@ async function UpdateItem(req,res,next){
 
 
 
+async function SearchTheItems(req,res,next){
+
+    let employee_id     =  req.body.employee_id     || "";
+    let month           =  req.body.month    || "";
+
+
+    if(employee_id === "" || month === ""){
+        res.err="wrong parameters";
+        return next();
+    }
+
+    let Query = `SELECT * FROM ${tableName} `;
+    Query += `WHERE employee_id = ? `;
+    Query += `AND MONTH(entry_time) = ? `;
+    Query += `ORDER BY entry_time`;
+   
+     let values = [employee_id,month];
+
+    res.ok=false;
+    res.err="";
+    let rows= await GenObj_Mid.QueryExecSimpleReply(Query,values);
+
+    if(rows === false){
+        res.err="חלה תקלה, נא לנסות שנית";
+        return res.status(500).json({status:"ERROR",Query: Query,err:res.err,values:values});
+    }
+ 
+    res.ok=true;
+    req.ItemsData=rows;
+
+    next();
+}
+
 
 module.exports = {
-   
+    GetAllItems,
     AddItem,
     UpdateItem,
-  
+    SearchTheItems
 }
